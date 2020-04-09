39aee5284dbfe4ec40233fed99bb999cac090ee1
huangyichao0916 <51321585+huangyichao0916@users.noreply.github.com>
2020-04-09 21:13:58 +0800

    今天学习了递归的使用，以及js的文件对象的使用

---
+++ AI/car/index.js
@@ -0,0 +1,30 @@
+var fs = require('fs');  //node  file system       node自己的模块，不需要再install
+var path = require('path');
+
+// fs.readFile('textdd.txt',function(err,data){
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data.toString());
+// });
+
+function getDirFiles(startPath){
+    let result = [];
+    let files = fs.readdirSync(
+        path.join(process.cwd(),startPath)
+        );
+    for (let file of files) {
+        let stats = fs.statSync(
+            path.join(process.cwd(),startPath,file)
+            );
+        if (stats.isFile()) {
+            result.push(file);
+        }
+        if (stats.isDirectory()) {
+            
+        }
+    }
+    return result;
+}
+console.log(getDirFiles('src'));
