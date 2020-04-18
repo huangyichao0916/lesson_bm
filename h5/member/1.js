0600939017f4e199833e59d3dbe57f9c5ad6afdf
huangyichao0916 <51321585+huangyichao0916@users.noreply.github.com>
2020-04-18 15:25:14 +0800

    进行了一些完善

--- h5/member/1.js
+++ h5/member/1.js
@@ -1,16 +1,57 @@
 var membersEntity = [
     {
-        id: 1314,
+        id: '1314',
         name: 'hyc',
-        avatar: ''
+        avatar: '',
+        phone_number: '18339542812',
+        hometown: '南昌'
     },
     {
-        id: 520,
+        id: '520',
         name: 'huang',
-        avatar: ''
+        avatar: '',
+        phone_number: '18439349412',
+        hometown: '北京'
     },
 ];
-var tbody = document.querySelector('#member tbody');
-tbody.innerHTML = membersEntity.map(function (item) {
-    return "\n    <tr>\n    <td>" + item.id + "</td>\n    <td>" + item.name + "</td>\n    <td>" + item.avatar + "</td>\n    </tr>\n    ";
-}).join('');
+// 渲染函数，用于读取membersEntity的内容并渲染出来
+var apply = function () {
+    var tbody = document.querySelector('#member tbody');
+    return function () {
+        tbody.innerHTML = membersEntity.map(function (item) {
+            return "\n            <tr>\n            <td>" + item.id + "</td>\n            <td>" + item.name + "</td>\n            <td><img src=\"" + item.avatar + "\"></td>\n            <td>" + item.phone_number + "</td>\n            <td>" + item.hometown + "</td>\n            </tr>\n            ";
+        }).join('');
+    };
+}();
+apply();
+var form = document.getElementsByName('myForm')[0];
+// console.log(form);
+form.addEventListener('submit', function (event) {
+    //阻止默认事件
+    event.preventDefault();
+    //获取输入框里的值,获取完后就将input框里的内容清空
+    var inputID = document.getElementsByName('ID')[0].value;
+    document.getElementsByName('ID')[0].value = '';
+    var inputName = document.getElementsByName('name')[0].value;
+    document.getElementsByName('name')[0].value = '';
+    var inputAvatar = document.getElementsByName('avatar')[0].value;
+    document.getElementsByName('avatar')[0].value = '';
+    var inputPhoneNumber = document.getElementsByName('phone_number')[0].value;
+    document.getElementsByName('phone_number')[0].value = '';
+    var inputHomeTown = document.getElementsByName('hometown')[0].value;
+    document.getElementsByName('hometown')[0].value = '';
+    if (inputID && inputName && inputAvatar && inputPhoneNumber && inputHomeTown) {
+        var member = {
+            id: inputID,
+            name: inputName,
+            avatar: inputAvatar,
+            phone_number: inputPhoneNumber,
+            hometown: inputHomeTown
+        };
+        membersEntity.push(member);
+        apply();
+    }
+    else {
+        window.alert('不能输入空值');
+    }
+});
--- h5/member/1.ts
+++ h5/member/1.ts
@@ -1,29 +1,78 @@
-interface MemberEntity{
-    id:number;
-    name:string;
-    avatar:string;
+interface MemberEntity {
+    id: string;
+    name: string;
+    avatar: string;
+    phone_number: string;
+    hometown: string;
 }
 
-const membersEntity:MemberEntity[] = [
+const membersEntity: MemberEntity[] = [
     {
-        id : 1314,
-        name : 'hyc',
-        avatar : ''
+        id: '1314',
+        name: 'hyc',
+        avatar: '',
+        phone_number: '18339542812',
+        hometown: '南昌',
     },
     {
-        id : 520,
+        id: '520',
         name : 'huang',
-        avatar:''
+        avatar: '',
+        phone_number: '18439349412',
+        hometown: '北京',
     },
 
 ]
-const tbody = document.querySelector('#member tbody');
-tbody.innerHTML = membersEntity.map((item)=>{
-    return `
-    <tr>
-    <td>${item.id}</td>
-    <td>${item.name}</td>
-    <td>${item.avatar}</td>
-    </tr>
-    `
-}).join('');
+
+// 渲染函数，用于读取membersEntity的内容并渲染出来
+let apply = function(){
+    const tbody = document.querySelector('#member tbody');
+    return function(){
+        tbody.innerHTML = membersEntity.map((item) => {
+            return `
+            <tr>
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td><img src="${item.avatar}"></td>
+            <td>${item.phone_number}</td>
+            <td>${item.hometown}</td>
+            </tr>
+            `
+        }).join('');
+    }
+}();
+apply();
+const form = document.getElementsByName('myForm')[0];
+// console.log(form);
+form.addEventListener('submit',(event) => {
+    //阻止默认事件
+    event.preventDefault();
+
+    //获取输入框里的值,获取完后就将input框里的内容清空
+    const inputID = (document.getElementsByName('ID')[0] as HTMLInputElement).value;
+    (document.getElementsByName('ID')[0] as HTMLInputElement).value='';
+    const inputName = (document.getElementsByName('name')[0] as HTMLInputElement).value;
+    (document.getElementsByName('name')[0] as HTMLInputElement).value='';
+    const inputAvatar = (document.getElementsByName('avatar')[0] as HTMLInputElement).value;
+    (document.getElementsByName('avatar')[0] as HTMLInputElement).value='';
+    const inputPhoneNumber = (document.getElementsByName('phone_number')[0] as HTMLInputElement).value;
+    (document.getElementsByName('phone_number')[0] as HTMLInputElement).value='';
+    const inputHomeTown = (document.getElementsByName('hometown')[0] as HTMLInputElement).value;
+    (document.getElementsByName('hometown')[0] as HTMLInputElement).value='';
+    
+
+    if (inputID&&inputName&&inputAvatar&&inputPhoneNumber&&inputHomeTown) {
+        let member:MemberEntity = {
+            id: inputID,
+            name: inputName,
+            avatar: inputAvatar,
+            phone_number: inputPhoneNumber,
+            hometown: inputHomeTown,
+        }
+        membersEntity.push(member);
+        apply();
+    }
+    else{
+        window.alert('不能输入空值');
+    }
+})
