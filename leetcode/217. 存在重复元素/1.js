f21bb4716b8f72bb19eb668e982391131ee54b6f
huangyichao0916 <51321585+huangyichao0916@users.noreply.github.com>
2020-04-28 21:58:35 +0800

    217. 存在重复元素

---
+++ leetcode/217. 存在重复元素/1.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort(function(a,b){
+        return a - b;
+    })
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i-1]) {
+            return true;
+        }
+    }
+    return false;
+};
