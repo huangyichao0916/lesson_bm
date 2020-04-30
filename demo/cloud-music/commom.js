fcc3fd7e44f4e67eb3598b69a8c6d25e9858b2f7
huangyichao0916 <939121836@qq.com>
2020-04-30 11:28:28 +0800

    将swiper的初始化放到了then里面，让loop生效

--- demo/cloud-music/commom.js
+++ demo/cloud-music/commom.js
@@ -32,26 +32,31 @@ window.onload = function () {
             //     // console.log(oDiv);
             //     swiper_wrapper.appendChild(oDiv);
             // })
+            new Swiper('.swiper-container', {
+                autoplay:{
+                    disableOnInteraction:false,
+                    delay:2500
+                },
+                direction: 'horizontal', // 垂直切换选项
+                loop: true, // 循环模式选项
+            
+                // 如果需要分页器
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            
+                // 如果需要前进后退按钮
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            
+                // 如果需要滚动条
+                // scrollbar: {
+                //     el: '.swiper-scrollbar',
+                // },
+            })
         })
 }
-new Swiper('.swiper-container', {
-    direction: 'horizontal', // 垂直切换选项
-    loop: true, // 循环模式选项
 
-    // 如果需要分页器
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // 如果需要前进后退按钮
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // 如果需要滚动条
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-})
 
