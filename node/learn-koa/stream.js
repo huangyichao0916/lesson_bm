1f085b89c521fd2b990b4caab8e2a25c96318256
huangyichao0916 <939121836@qq.com>
2020-05-11 22:23:34 +0800

    进行了一些完善

--- node/learn-koa/stream.js
+++ node/learn-koa/stream.js
@@ -3,8 +3,16 @@ const koa = require('koa');
 const app = new koa();
 
 const main = ctx => {
-    ctx.response.type = 'html';  //响应头
-    ctx.response.body = fs.createReadStream('index.html');
+    if (ctx.request.url === '/ch.jpeg') {
+        ctx.response.type = 'image';
+        ctx.response.body = fs.createReadStream('ch.jpeg');
+    }else if (ctx.request.url === '/style.css') {
+        ctx.response.type = 'css';
+        ctx.response.body = fs.createReadStream('style.css');
+    }else{
+        ctx.response.type = 'html';  //响应头
+        ctx.response.body = fs.createReadStream('index.html');
+    }
 }
 
 app.use(main);
