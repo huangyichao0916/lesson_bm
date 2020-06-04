79f1c79105318a1df653faeb0fd1a31a264dc4c9
huangyichao0916 <939121836@qq.com>
2020-06-04 10:55:53 +0800

    redux学习

---
+++ react/shopping-cart/src/reducers/cart.js
@@ -0,0 +1,13 @@
+const initialState = {
+    addedIds : [],
+    quantityById: {},
+}
+
+const cart = (state = initialState , action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default cart;
---
+++ react/shopping-cart/src/reducers/products.js
@@ -0,0 +1,13 @@
+// const initialState = {
+//     addedIds : [],
+//     quantityById: {},
+// }
+
+const products = (state = [] , action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default products;
