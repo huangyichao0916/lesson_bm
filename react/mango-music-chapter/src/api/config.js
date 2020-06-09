4265abaa84e93c9c9bfe123f15d8e58f5d73bb65
huangyichao0916 <939121836@qq.com>
2020-06-09 10:36:11 +0800

    一些改善

---
+++ react/mango-music-chapter/src/api/config.js
@@ -0,0 +1,20 @@
+const URL = {
+    carousel : "",
+    newalbum : '',
+    rankingList: "https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg",
+}
+
+const PARAM = {
+    format: "jsonp",
+    inCharset: "utf-8",
+    outCharset: "utf-8",
+    notice: 0,
+}
+
+const OPTION = {
+    param: "jsonpCallback",
+    prefix: "callback"
+}
+  
+  
+export { URL, PARAM, OPTION };
