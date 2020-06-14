7cde047ebca82b91cd20cb6767694d62ef7d9ad5
huangyichao0916 <939121836@qq.com>
2020-06-14 17:33:21 +0800

    进一步完善

---
+++ react/mango-music-chapter/src/components/album/Album.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './album.styl';
+
+class Album extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Album
+            </div>
+        );
+    }
+}
+ 
+export default Album;
--- react/mango-music-chapter/src/components/recommend/Recommend.jsx
+++ react/mango-music-chapter/src/components/recommend/Recommend.jsx
@@ -4,31 +4,17 @@ import Swiper from 'swiper';
 import "swiper/css/swiper.min.css";
 import Loading from '../../common/loading/Loading';
 import Scroll from '@/common/scroll/Scroll';
+import * as AlbumModel from '@/model/album';  
 
-
-// 应用中很多图片
-// import Lazyload from 'react-lazyload'; // 图片延迟加载
-// 1. 路由   
-// 2. redux
-// 3. 切页面+ js 
-// 4. 生命周期 + api 
-// 5. 公共组件
-
-
-// 所有的数据请求都放到api目录下
 import { getNewAlbum }  from '../../api/recommend';
 import LazyLoad, { forceCheck } from 'react-lazyload';
 
+import Album from '@/components/album/Album';
+import {Route} from 'react-router-dom'
 
-// 1. 幻灯片， swiper
-// 2. 加入 swiper功能
-// 数据   { src, link}
 class Recommend extends React.Component {
   constructor() {
     super()
-    // 这个组件 state , 属于这个组件， 不属于其他组件， 
-    // 1. 用假数据 把页面先做出
-    // 2. 未来再改成接口
     this.state = {
       refreshScroll: false,
       newAlbums: [], /* 数据驱动的界面 */
@@ -48,6 +34,14 @@ class Recommend extends React.Component {
       }] // 幻灯片  没有必要去redux 
     }
   }
+
+  componentDidUpdate() {
+    //组件更新后，如果实例化了better-scroll并且需要刷新就调用refresh()函数
+    if (this.bScroll && this.props.refresh === true) {
+        this.bScroll.refresh();
+    }
+  }
+
   componentDidMount() {
     new Swiper(".slider-container", {
       loop: true,
@@ -60,46 +54,62 @@ class Recommend extends React.Component {
       }
     })
 
-
     // 获取最新专辑 功能的封装
     // fetch  低级的
     getNewAlbum() /**promise */
       .then(res => {
-        // console.log(res)
+        let albumList = res.albumlib.data.list;
+
+        // model 
+        // albumList.sort()
         this.setState({
           loading: false,
-          newAlbums: res.albumlib.data.list
+          newAlbums: albumList
+        }, () => {
+          this.setState({
+            refreshScroll: true
+          })
         })
       })
-    // setTimeout(() => {
-    //   this.setState({
-    //     loading: false
-    //   })
-    // }, 3000)
+  }
+  toAlbumDetail(url){
+    console.log(url);
+    this.props.history.push({
+      pathname:url,
+    })
   }
   render() {
-    // 切页面
-    // console.log(this.state.newAlbums);
-    let albums = this.state.newAlbums.map(item => (
-      <div className="album-wrapper" key={item.album_id}>
+    let {match} = this.props;
+    let albums = this.state.newAlbums.map(item => {
+      let album = AlbumModel.createAlbumByList(item);
+      // console.log(album);
+      return (
+      <div className="album-wrapper" 
+        key={album.id}
+        onClick={() => this.toAlbumDetail(`${match.url}/${album.mId}`)}
+      >
         <div className="left">
-          <img src="https://qpic.y.qq.com/music_cover/Iia3lpoTl2hPXtBpjHk9QiaqUplwwdfZdf48EHsTO7PgO18LnQ74BPdQ/300?n=1" alt={item.album_name} width="100%" height="100%"/>
+          <LazyLoad height={60}>
+            <img src={album.img} alt={item.name} width="100%" height="100%"/>
+          </LazyLoad>
         </div>
         <div className="right">
           <div className="album-name">
-            {item.album_name}
+            {album.name}
           </div>
           <div className="singer-name">
-            {item.singers[0].singer_name}
+            {album.singer}
           </div>
           <div className="public-time">
-            {item.public_time}
+            {album.publicTime}
           </div>
         </div>
       </div>
-    ))
+      )
+    })
     return (
       <div className="music-recommend">
+       
           <div className="slider-container">
             <div className="swiper-wrapper">
               {
@@ -116,13 +126,12 @@ class Recommend extends React.Component {
             </div>
             <div className="swiper-pagination"></div>
           </div>
-        
         <Scroll
           refresh={this.state.refreshScroll}
-          onScroll={(e) => {
-            console.log(e);
+          onScroll={(event) => {
+            // console.log(e);
             forceCheck();
-        }}>  
+          }}>
           <div className="album-container">
             <h1 className="title">最新专辑</h1>
             <div className="album-list">
@@ -131,10 +140,10 @@ class Recommend extends React.Component {
           </div>
         </Scroll>
         <Loading show={this.state.loading} title="正在加载..."/>
+        <Route path={`${match.url}/:id'`} component={Album} />
       </div>
     )
   }
 }
-
 
 export default Recommend;
