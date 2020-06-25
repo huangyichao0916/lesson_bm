6f41a40f75e8713fa1039416db7a839b77a58913
huangyichao0916 <939121836@qq.com>
2020-06-25 16:06:33 +0800

    进一步完善

--- react/react-mobx-realworld/src/pages/home/index.jsx
+++ react/react-mobx-realworld/src/pages/home/index.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Row, Col, Tag,Spin } from 'antd';
 import { inject, observer } from 'mobx-react';
 import { Pagination } from 'antd';
-import { Layout, Row, Col,Tag } from 'antd';
-
 
 const { TabPane } = Tabs;
-// 想要哪个页面的数据 注入谁
-@inject('articleStore')
+// 想要哪个页面的数据 注入谁   // 不需要再写 connect mapStatetoProps mapDispatch
+@inject('articleStore')   
 @observer
 class Home extends Component {
   componentDidMount() {
@@ -20,17 +18,21 @@ class Home extends Component {
     // 3 offset 2
     this.props.articleStore.getArticle('all', page - 1);
   }
+
   render() {
-    const { total, LIMIT, articles,handleTabChange,tags } = this.props.articleStore
+    
+    const { total, LIMIT, articles, handleTabChange, tags ,isLoading } = this.props.articleStore
     console.log(total, LIMIT)
     return (
       <div>
         <Row>
           <Col span={19}>
-            <Tabs defaultActiveKey={'all'} onChange={handleTabChange}>
-              {Object.keys(articles).map((tag, i) => {
-                return (
-                  <TabPane key={tag} tab={tag}>
+          <Tabs defaultActiveKey={'all'} onChange={handleTabChange}>
+            {/* 点击 tab 请求内容 */}
+            {Object.keys(articles).map((tag, i) => {
+              return (
+                <TabPane key={tag} tab={tag}>
+                  <Spin tip="正在加载中..." spinning={isLoading}>
                     {
                       articles[tag].map((article, i) => {
                         return (
@@ -45,24 +47,26 @@ class Home extends Component {
                         )
                       })
                     }
-                  </TabPane>
-                )
-              })}
-            </Tabs>
-          </Col >
-          <Col span={5}>
-              {tags.map((tag,i) => <Tag key={i}> {tag} </Tag>)}
-          </Col>
+                  </Spin>
+                </TabPane>
+              )
+            })}
+          </Tabs>
+          <Pagination
+            onChange={this.handlePaginationChange}
+            total={total}
+            pageSize={LIMIT}
+            defaultCurrent={1} />
+        </Col>
+        <Col span={5}>
+          {tags.map((tag, i) => {
+            return <Tag key={i}>{tag}</Tag>
+          })}
+        </Col>
         </Row>
-        <Pagination
-          onChange={this.handlePaginationChange}
-          total={total}
-          pageSize={LIMIT}
-          defaultCurrent={1} />
       </div>
     );
   }
 }
-
 
 export default Home;
