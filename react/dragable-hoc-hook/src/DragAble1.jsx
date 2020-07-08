d5dad4d4b6a7479cddcdc5b6e3d3e779cee456e0
huangyichao0916 <939121836@qq.com>
2020-07-08 15:25:01 +0800

    react hoc的学习

---
+++ react/dragable-hoc-hook/src/DragAble1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DragAble1 extends Component {
+    state = {}
+    render() { 
+        return (
+            <div>
+                DragAble1
+            </div>
+        )
+    }
+}
+ 
+export default DragAble1;
