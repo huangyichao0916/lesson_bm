a091006f72e5140c097f2827dba7cd51d29ce6ae
huangyichao0916 <939121836@qq.com>
2020-07-08 16:03:16 +0800

    高阶组件的学习

--- react/dragable-hoc-hook/src/App.js
+++ react/dragable-hoc-hook/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 
 function App() {
@@ -7,6 +7,19 @@ function App() {
       react
     </div>
   );
+}
+
+function withDragAble(Com) {
+  class WithDragAble extends Component{
+
+    render(){
+      return(
+        <div>
+          <Com />
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
