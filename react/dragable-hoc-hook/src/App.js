fdc64a550cb573873abf36972a8eab5ff897336b
huangyichao0916 <939121836@qq.com>
2020-07-09 09:58:11 +0800

    一些完善

--- react/dragable-hoc-hook/src/App.js
+++ react/dragable-hoc-hook/src/App.js
@@ -10,11 +10,41 @@ function App() {
 }
 
 function withDragAble(Com) {
-  class WithDragAble extends Component{
-
-    render(){
-      return(
-        <div>
+  return class extends Component {
+    state = {
+      left: 0,
+      top: 0
+    }
+    handleDown= (e) => {
+      var startX = e.clientX;
+      var startY = e.clientY;
+      let obj = e.target.getBoundingClientRect();
+      // 重置起点
+      this.startX = startX - obj.x;   // obj.left  // 0
+      this.startY = startY - obj.y;   // 0
+      document.addEventListener('mousemove', this.handleMove);
+      document.addEventListener('mouseup', this.handleUp);
+    }
+    handleMove = (e) => {
+      var newX = e.clientX;
+      var newY = e.clientY;
+      const diffX = newX - this.startX;
+      const diffY = newY - this.startY;
+      this.setState({
+        left: diffX,
+        top: diffY
+      })
+    }
+    handleUp = () => {
+      document.removeEventListener('mousemove', this.handleMove);
+      document.removeEventListener('mouseup', this.handleUp);
+    }
+    render() {
+      const { left, top} = this.state;
+      return (
+        <div style={{left, top}} className="dragable"
+        onMouseDown={this.handleDown}
+        >
           <Com />
         </div>
       )
