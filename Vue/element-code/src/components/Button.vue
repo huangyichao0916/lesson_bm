2c1d16cc1435486c1748943e5ec0b69a45beae64
huangyichao0916 <939121836@qq.com>
2020-08-25 22:38:26 +0800

    vue study

---
+++ Vue/element-code/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        button
+    </div>
+</template>
+
+<script>
+export default {
+    "components": {
+        "el-button": ElButton
+    }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
