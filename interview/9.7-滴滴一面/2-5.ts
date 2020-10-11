4c0777e73b44d80d88e6ad6ba0f8eea84ccf239d
huangyichao0916 <939121836@qq.com>
2020-10-11 22:06:24 +0800

    study

--- interview/9.7-滴滴一面/2-5.ts
+++ interview/9.7-滴滴一面/2-5.ts
@@ -1,29 +1,110 @@
-// function deepCopy(obj1) {
-//     var obj2 = Array.isArray(obj1) ? [] : {};
-//     if (obj1 && typeof obj1 === "object") {
-//         for (var i in obj1) {
-//             if (obj1.hasOwnProperty(i)) {
-//                 // 如果子属性为引用数据类型，递归复制
-//                 if (obj1[i] && typeof obj1[i] === "object") {
-//                     obj2[i] = deepCopy(obj1[i]);
-//                 } else {
-//                     // 如果是基本数据类型，只是简单的复制
-//                     obj2[i] = obj1[i];
-//                 }
-//             }
-//         }
-//     }
-//     return obj2;
-// }
+//递归实现深拷贝  但是未解决循环引用的问题
+function deepCopy(obj: any) {
+    // const map:Map<any,any> = new Map();
 
-//递归实现深拷贝  用map来避免相互引用造成死循环的问题
-function deepCopy(obj:any) {
-    const map:Map<any,any> = new Map();
+    function helper(myObj: any) {
+        if (typeof myObj !== 'object') {
+            return myObj;
+        }
+        const isArr: boolean = Array.isArray(myObj)
+        const resObj: any = isArr ? [] : {};
+        // if (map.get(myObj)) {
+        //     return
+        // }
+        // map.set(myObj,true)
 
-    function helper(myObj:any) {
-        map.set(myObj,true)
+        if (isArr) {
+            for (let i = 0; i < myObj.length; i++) {
+                const element = myObj[i];
+                resObj[i] = deepCopy(element)
+            }
+        } else {
+            for (const key in myObj) {
+                resObj[key] = deepCopy(myObj[key])
+            }
+        }
+        return resObj;
     }
+
+    return helper(obj)
 }
+// let a = {
+//     a: 3,
+//     b: {
+//         a: 3,
+//         c: 5
+//     }
+// }
+// let b = deepCopy(a)
+// console.log(a)
+// console.log(b)
+// console.log(a.b == b.b)
+
+
+
+//解决了循环引用的问题
+function deepCopy2(obj) {
+    const map = new Map();
+
+    function helper(myObj) {
+        if (typeof myObj !== 'object') {
+            return myObj;
+        }
+        if (map.get(myObj)) {
+            return map.get(myObj)
+        }
+        const isArr = Array.isArray(myObj)
+        const resObj = isArr ? [] : {};
+        map.set(myObj, resObj)
+
+        if (isArr) {
+            for (let i = 0; i < myObj.length; i++) {
+                const element = myObj[i];
+                resObj[i] = helper(element)
+            }
+        } else {
+            for (const key in myObj) {
+                resObj[key] = helper(myObj[key])
+            }
+        }
+        return resObj;
+    }
+
+    return helper(obj)
+}
+let obj2: any = {
+    b: 3,
+    c: {
+        a: {
+            a: 3,
+            x: 3
+        }
+    }
+};
+let xxx: any = obj2;
+obj2.b = xxx;
+let objj: any = deepCopy2(obj2)
+console.log(obj2)
+
+
 
 
 // JSON.stringify + JSON.parse实现深拷贝
+function deepCopy3(obj: any) {
+    return JSON.parse(JSON.stringify(obj))
+}
+
+let obj: any = {
+    b: {
+        a: 1,
+        c: 3,
+    },
+    c: {
+        a: {
+            a: 3,
+            x: 3
+        }
+    }
+}
+// let obj2:any = deepCopy3(obj)
+// console.log(obj2.c == obj.c)
