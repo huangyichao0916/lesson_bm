a28a74d1ecc5383728ea24be01f32f6eb31587c0
huangyichao0916 <939121836@qq.com>
2020-11-16 17:00:16 +0800

    study

--- browser/跨域解决方案/jsonp/server.js
+++ browser/跨域解决方案/jsonp/server.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 http.createServer((req, res) => {
     if (req.url === '/') {
         fs.createReadStream('./1.html').pipe(res);
-    } else if(req.url.includes('/api')){
-        res.end(`foo({code:0,msg:'success'})`)
-    }else {
+    } else if (req.url.includes('/api')) {
+        const callBackName = req.url.match(/callback\=(\w+)/) ? req.url.match(/callback\=(\w+)/)[1] : null;
+        res.end(`${callBackName}({code:0,msg:'success'})`)
+        // res.end('asdasd')
+    } else {
         res.end('');
     }
 })
