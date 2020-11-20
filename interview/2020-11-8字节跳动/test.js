89698e2eb20a76654b5b3ef402e3ea7f70ecdfcf
huangyichao0916 <939121836@qq.com>
2020-11-20 13:37:08 +0800

    study

--- interview/2020-11-8字节跳动/test.js
+++ interview/2020-11-8字节跳动/test.js
@@ -1,10 +1,39 @@
-new Promise((resolve, reject) => {
-    reject(1)
-})
-    .catch(() => {
-        console.log(2)
-    })
-    .then(
-        () => console.log(3),
-        (v) => console.log(v)
-    )
+const func = ({ getState }) => next => (action) => {
+    // Exit early if predicate function returns 'false'
+    if (typeof predicate === 'function' && !predicate(getState, action)) {
+        return next(action);
+    }
+
+    const logEntry = {};
+
+    logBuffer.push(logEntry);
+
+    logEntry.started = timer.now();
+    logEntry.startedTime = new Date();
+    logEntry.prevState = stateTransformer(getState());
+    logEntry.action = action;
+
+    let returnedValue;
+    if (logErrors) {
+        try {
+            returnedValue = next(action);
+        } catch (e) {
+            logEntry.error = errorTransformer(e);
+        }
+    } else {
+        returnedValue = next(action);
+    }
+
+    logEntry.took = timer.now() - logEntry.started;
+    logEntry.nextState = stateTransformer(getState());
+
+    const diff = loggerOptions.diff && typeof diffPredicate === 'function'
+        ? diffPredicate(getState, action)
+        : loggerOptions.diff;
+
+    printBuffer(logBuffer, Object.assign({}, loggerOptions, { diff }));
+    logBuffer.length = 0;
+
+    if (logEntry.error) throw logEntry.error;
+    return returnedValue;
+};
